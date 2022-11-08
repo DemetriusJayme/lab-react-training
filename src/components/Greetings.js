c93430f609e32269aac65c93e4fafb274d40888f
DemetriusJayme <115664765+DemetriusJayme@users.noreply.github.com>
2022-11-08 14:43:47 -0300

    done

---
+++ src/components/Greetings.js
@@ -0,0 +1,7 @@
+import App from '../App';
+
+function Greetings(props) {
+  return <div></div>;
+}
+
+export default Greetings;
---
+++ src/components/IdCard.js
@@ -0,0 +1,10 @@
+function IdCard(lastName, firstName, gender, height, birth, picture) {
+  return (
+    <div>
+      <img src={picture} alt="profile pic" />
+      <div>
+        <p>First Name: </p>;
+      </div>
+    </div>
+  );
+}
